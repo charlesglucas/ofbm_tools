d058ae164b339c0295768313291efd3f30e6aba8
Charles-Gérard Lucas <cglucas1@gmail.com>
2023-05-10 10:32:36 +0200

    Add files via upload

---
+++ include/BSChi2test.m
@@ -0,0 +1,22 @@
+function [test] = BSChi2test(est,alpha)
+%  Input:     est:     structure of estimation method 
+%             alpha:   significance level of the tests
+%
+%  Output:    test:    structure of folded normal testing procedures
+%                       - T : chi2 statistic
+%                       - dec : decision
+%                       - pval : p-value
+%
+% Charles-Gérard Lucas, ENS Lyon, 2021
+
+P = length(est.lambda);
+
+% compute approximate chi2 test
+test.invSigma = inv(cov(est.lambdaBS));
+cL = est.lambda - mean(est.lambda);
+test.T=cL*test.invSigma*cL';
+test.dec = test.T >= chi2inv(1-alpha,P-1);
+test.pval = 1 - chi2cdf(test.T,P-1);
+
+end
+
