7dab6e3ed7d050467677a7ba153f5563ad8b5dc1
Charles-Gérard Lucas <cglucas1@gmail.com>
2023-07-14 12:49:54 -0400

    Add files via upload

---
+++ examples/demo_biasCorrectedEstim.m
@@ -0,0 +1,27 @@
+%% estimate multivariate self-similarity exponents
+% CGL, October 2021.
+
+clc
+clear all
+close all 
+format compact
+
+addpath('../include/')
+load('../data/result_estimbc_sizeH6.mat')
+
+%% Estimation
+paramsEst.FBM = 1;
+paramsEst.Nwt = 2;
+paramsEst.j1 = 5;
+paramsEst.j2 = 10;
+paramsEst.Jref = paramsEst.j2;
+paramsEst.FigNum = 1; 
+paramsEst.Jrefplot = 1;
+paramsEst.wtype = 1;
+paramsEst.NB = 0;
+paramsEst.LB = 0;
+paramsEst.nbcompmaxplot  = 6;
+
+[est,estbc] = OFBM_estimBC_BS(data,paramsEst);
+disp(['Classical H estimate:        [',num2str(est.h),']'])
+disp(['Bias corrected H estimate:   [',num2str(estbc.h),']'])
